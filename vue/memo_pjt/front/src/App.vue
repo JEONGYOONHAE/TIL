b7da0b398b94c3bcbe49d69f98fa3e4f21c349e4
YOONHAE <ue0828@naver.com>
2022-06-26 13:56:34 +0900

    memo_pjt

---
+++ vue/memo_pjt/front/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">
+    <homeComp/>
+  </div>
+</template>
+
+<script>
+import homeComp from './components/homeComp.vue'
+
+export default {
+  components: {
+    homeComp
+  }
+}
+</script>
+
+<style>
+</style>
